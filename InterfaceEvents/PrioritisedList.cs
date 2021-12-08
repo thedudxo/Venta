f71b8d3344a4f1f7902e7f6f1d57c3145edee966
thedudxo <isaacboon@gmail.com>
2021-12-09 10:40:06 +1300

    Keeping reference of the linked list node instead of the whole list, allows removing an item to be 0(log n)

--- InterfaceEvents/PrioritisedList.cs
+++ InterfaceEvents/PrioritisedList.cs
@@ -6,41 +6,51 @@ namespace DudCo.Events
     internal class PrioritisedList<T> : IEnumerable<KeyValuePair<int, LinkedList<T>>>
     {
         //similar concept to the contents and index sections of a book
-        SortedDictionary<int, LinkedList<T>> contents = new SortedDictionary<int, LinkedList<T>>();
-        Dictionary<T, (LinkedList<T>list, int priority)> indexes = new Dictionary<T, (LinkedList<T>,int)>();
+        SortedDictionary<int, LinkedList<T>> contents;
+        Dictionary<T, (LinkedListNode<T> node, int priority)> indexes;
+
+        internal PrioritisedList()
+        {
+            contents = new SortedDictionary<int, LinkedList<T>>();
+            indexes = new Dictionary<T, (LinkedListNode<T>, int)>();
+        }
 
         public void Add(T item, int priority) //O(log n)
         { 
             //invert so the dictionary is sorted the right way around
             priority *= -1;
-            LinkedList<T> items = null;
+            LinkedListNode<T> node = null;
 
             bool priorityAllreadyExists = contents.ContainsKey(priority); //O(log n)
             if (priorityAllreadyExists)
             {
-                items = contents[priority]; //O(log n)
-                items.AddLast(item); //O(1)
+                var items = contents[priority]; //O(log n)
+                node = items.AddLast(item); //O(1)
             }
             else
-                items = CreateAndAddToNewItemsList(item, priority); //O(log n)
+                node = CreateAndAddToNewItemsList(item, priority); //O(log n)
 
-            var index = (items, priority);
+            var index = (node, priority);
             indexes[item] = index; //O(1)
         }
 
-        private LinkedList<T> CreateAndAddToNewItemsList(T item, int prio) //O(log n)
+        private LinkedListNode<T> CreateAndAddToNewItemsList(T item, int prio) //O(log n)
         {
             LinkedList<T> items = new LinkedList<T>();
-            items.AddLast(item); //O(1)
+            LinkedListNode<T> node;
+
+            node = items.AddLast(item); //O(1)
             contents.Add(prio, items); //O(log n)
-            return items;
+
+            return node;
         }
 
-        public void Remove(T item) //O(n)
+        public void Remove(T item) //O(log n)
         {
-            LinkedList<T> items = indexes[item].list; //O(1)
+            LinkedListNode<T> node = indexes[item].node; //O(1)
+            LinkedList<T> items = node.List; 
 
-            items.Remove(item); //O(n)
+            items.Remove(item); //O(1)
 
             if (items.Count == 0)
             {
