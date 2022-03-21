325885b3691855004b57007c0bc1751e3c787f8e
thedudxo <isaacboon@gmail.com>
2022-03-22 12:34:16 +1300

    PriorityDictionary

---
+++ InterfaceEvents/PriorityDictionary.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace DudCo.Events
+{
+    public class PriorityDictionary
+    {
+        readonly Dictionary<Type, int> priorities = new Dictionary<Type, int>();
+
+        public void Add(Type type, int priorty) => priorities.Add(type, priorty);
+        public void Add<T>(int priority) => priorities.Add(typeof(T), priority);
+        public bool ContainsKey(Type t) => priorities.ContainsKey(t);
+        public int PriorityOf<T>() => priorities[typeof(T)];
+        public int PriorityOf(Type type) => priorities[type];
+        public int this[Type t] => priorities[t];
+        public int After<T>() => PriorityOf<T>() - 1;
+        public int Before<T>() => PriorityOf<T>() + 1;
+    }
+}
---
+++ Tests/Tests/PriorityDictionaryTests.cs
@@ -0,0 +1,174 @@
+﻿using NUnit.Framework;
+using DudCo.Events;
+
+namespace Tests.PriorityDictionaries
+{
+    public class PriorityDictionaryTests
+    {
+        PriorityDictionary priorites = new PriorityDictionary();
+
+        [SetUp]
+        public void Setup()
+        {
+            priorites = new PriorityDictionary();
+        }
+
+        class itemA { }
+        class itemB { }
+        class itemC { }
+
+        class Add : PriorityDictionaryTests
+        {
+            [Test]
+            public void Contains_AdddedItemGeneric()
+            {
+                priorites.Add<itemA>(0);
+
+                Assert.That(priorites.ContainsKey(typeof(itemA)));
+            }
+
+            [Test]
+            public void Contains_AdddedItemGeneric_CorrectPriority()
+            {
+                const int priority = 1;
+
+                priorites.Add<itemA>(priority);
+
+                Assume.That(priorites.ContainsKey(typeof(itemA)));
+                Assert.AreEqual(priority, priorites[typeof(itemA)]);
+            }
+
+            [Test]
+            public void Contains_AdddedItemType()
+            {
+                priorites.Add(typeof(itemA), 0);
+
+                Assert.That(priorites.ContainsKey(typeof(itemA)));
+            }
+
+            [Test]
+            public void Contains_AdddedItemType_CorrectPriority()
+            {
+                const int priority = 1;
+
+                priorites.Add(typeof(itemA), priority);
+
+                Assume.That(priorites.ContainsKey(typeof(itemA)));
+                Assert.AreEqual(priority, priorites[typeof(itemA)]);
+            }
+
+            [Test]
+            public void AddExistingItem_ThrowsArgumentException()
+            {
+                priorites.Add<itemA>(0);
+
+                Assert.Throws<System.ArgumentException>(
+                    () => priorites.Add<itemA>(0)
+                    );
+            }
+        }
+
+        class PriorityOf : PriorityDictionaryTests
+        {
+            [Test]
+            public void PriorityOfGeneric_AddedItem_Correct()
+            {
+                const int priority = 1;
+
+                priorites.Add<itemA>(priority);
+
+                Assert.AreEqual(priority, priorites.PriorityOf<itemA>());
+            }
+
+            [Test]
+            public void PriorityOfType_AddedItem_Correct()
+            {
+                const int priority = 1;
+
+                priorites.Add<itemA>(priority);
+
+                Assert.AreEqual(priority, priorites.PriorityOf(typeof(itemA)));
+            }
+
+            [Test]
+            public void PriortiyOfType_NotAddeditem_ThrowsKeyNotFoundExceptionException()
+            {
+                Assert.Throws<System.Collections.Generic.KeyNotFoundException>(
+                    () => priorites.PriorityOf(typeof(itemA)) 
+                    );
+            }
+
+
+            [Test]
+            public void PriortiyOfGeneric_NotAddeditem_ThrowsKeyNotFoundExceptionException()
+            {
+                Assert.Throws<System.Collections.Generic.KeyNotFoundException>(
+                    () => priorites.PriorityOf<itemA>()
+                    );
+            }
+        }
+
+        class Indexer : PriorityDictionaryTests
+        {
+            [Test]
+            public void TypeIndexerOf_AddedItem_Correct()
+            {
+                const int priority = 5;
+
+                priorites.Add<itemA>(priority);
+
+                Assert.AreEqual(priority, priorites[typeof(itemA)]);
+            }
+
+            [Test]
+            public void TypeIndexerOf_NotAddedItem_ThrowsKeyNotFoundExceptionException()
+            {
+                Assert.Throws<System.Collections.Generic.KeyNotFoundException>(
+                () => _= priorites[typeof(itemA)]
+                );
+            }
+        }
+
+        class AddAfter : PriorityDictionary
+        {
+            [Test]
+            public void AddAfter_PreviousItem_HasLowerPriority()
+            {
+                Add<itemA>(0);
+
+                Add<itemB>(After<itemA>());
+
+                Assert.Less(PriorityOf<itemB>(), PriorityOf<itemA>());
+            }
+
+            [Test]
+            public void AddAfter_NotFoundItem_ThrowsKeyNotFoundExceptionException()
+            {
+                Assert.Throws<System.Collections.Generic.KeyNotFoundException>(
+                    () => Add<itemB>(After<itemA>())
+                    );
+            }
+        }
+
+        class AddBefore : PriorityDictionary
+        {
+            [Test]
+            public void AddBefore_PreviousItem_HasGreaterPriority()
+            {
+                Add<itemA>(0);
+
+                Add<itemB>(Before<itemA>());
+
+                Assert.Greater(PriorityOf<itemB>(), PriorityOf<itemA>());
+            }
+
+            [Test]
+            public void AddBefore_NotFoundItem_ThrowsKeyNotFoundExceptionException()
+            {
+                Assert.Throws<System.Collections.Generic.KeyNotFoundException>(
+                    () => Add<itemB>(Before<itemA>())
+                    );
+            }
+        }
+    }
+}
